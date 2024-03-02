1e21ed8fd2c3edf77dc53d50888b65d31fd1f9bf
Nazarii Pylypenko <nazariipylypenko22@gmail.com>
2024-03-02 13:54:44 +0200

    Props practice task

--- index.js
+++ index.js
@@ -20,4 +20,22 @@ const parentElement = React.createElement('section', {}, component, component2);
 
 const root = document.querySelector('#root');
 
-ReactDOM.render(parentElement, root);
+// ReactDOM.render(parentElement, root);
+
+/*
+
+*/
+
+class GreetingsComponent extends React.Component {
+    render() {
+        const h1 = React.createElement('h1', {}, `Hello, ${this.props.name}`)
+        return h1;
+    }
+}
+
+const componentName = React.createElement(GreetingsComponent, {name: 'Josh'});
+const componentName2 = React.createElement(GreetingsComponent, {name: 'Daniel'});
+
+const parentElement2 = React.createElement('section', {}, componentName, componentName2);
+
+ReactDOM.render(parentElement2, root);
